a7e5cf3fb2c938bdd0c0eac9ae81634636247f50
LightLord <stevensah88@gmail.com>
2023-11-20 11:15:12 -0800

    feat preview for all genres

--- scripts/study-music.js
+++ scripts/study-music.js
@@ -8,7 +8,7 @@ const getLofi = async () => {
   const options = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "getyourown",
+      "X-RapidAPI-Key": "788e17aae3msh8c510bbeab7549ep1020c1jsndd7a758904a9",
       "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
     },
   };
@@ -17,23 +17,32 @@ const getLofi = async () => {
   const result = await response.json();
   return result;
 };
+
 const phonkMusic = [
   {
     title: "1 Hour of PHONK/MEMPHIS/808/COWBELL | EPISODE 4",
     link: "https://www.youtube.com/watch?v=049F3eoi98k",
+    thumbnail:
+      "https://i.ytimg.com/vi/049F3eoi98k/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCNMufsPOW2rwOokndn-zTvOXPHBg",
   },
   {
     title:
       "Phonk music 2023 ※ Aggressive Drift Phonk ※ Murder In My Mind / IN THE CLUB / RAVE / NEON BLADE",
     link: "https://www.youtube.com/watch?v=G2uGZ9Bt8JU&pp=ygUFcGhvbms%3D",
+    thumbnail:
+      "https://i.ytimg.com/vi/G2uGZ9Bt8JU/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLA700NAJfTzvUyrYD9HJPQVQGOnDw",
   },
   {
     title: "Top 50 Phonk Songs - Best of Phonk",
     link: "https://www.youtube.com/watch?v=8WtRKHwVROs&t=558s&pp=ygUFcGhvbms%3D",
+    thumbnail:
+      "https://i.ytimg.com/vi/8WtRKHwVROs/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLConghaEXcQrI1jtz-uRt1yYqXLtQ",
   },
   {
     title: "Songs that boost you into GODMODE 🤯💎",
     link: "https://www.youtube.com/watch?v=KzKqzvBAhTs&pp=ygUQdmlkZW8gZ2FtZSBtdXNpYw%3D%3D",
+    thumbnail:
+      "https://i.ytimg.com/vi/KzKqzvBAhTs/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDaCEGaWqOdV7UCAkJnOjeOAYsQ-w",
   },
 ];
 const videoGameMusic = [
@@ -41,15 +50,21 @@ const videoGameMusic = [
     title:
       "calm nintendo video game music for studying, sleep, work while it's raining",
     link: "https://www.youtube.com/watch?v=sA0qrPOMy2Y",
+    thumbnail:
+      "https://i.ytimg.com/vi/sA0qrPOMy2Y/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCAA4SzSrXHAQkTcCjAwXvr8s2f3A",
   },
   {
     title: "Zelda Music To Relax/Study/Work/Game",
     link: "https://www.youtube.com/watch?v=MXDF0wVcWfA&pp=ygUQdmlkZW8gZ2FtZSBtdXNpYw%3D%3D",
+    thumbnail:
+      "https://i.ytimg.com/vi/MXDF0wVcWfA/hqdefault.jpg?sqp=-oaymwE9COADEI4CSFryq4qpAy8IARUAAAAAGAElAADIQj0AgKJDeAHwAQH4Ac4GgALgA4oCDAgAEAEYNiBlKF4wDw==&rs=AOn4CLBQiBvv2JFFi4xuWslvGTSrdSYwOw",
   },
   {
     title:
       "relaxing music winter video game 2 hour (mostly nintendo) ❄️ w/ snowfall ambience",
     link: "https://www.youtube.com/watch?v=3nny-oOtMfs&pp=ygUQdmlkZW8gZ2FtZSBtdXNpYw%3D%3D",
+    thumbnail:
+      "https://i.ytimg.com/vi/3nny-oOtMfs/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLChthBJVnPEFVGWBNfv1d-Bw3Xs5g",
   },
 ];
 
@@ -71,6 +86,7 @@ if (music === "Lofi") {
     phonkMusic.map((phonk) => ({
       name: phonk.title,
       description: phonk.link,
+      preview: `<img src=${phonk.thumbnail}>`,
     }))
   );
   open(choice.link);
@@ -80,6 +96,7 @@ if (music === "Lofi") {
     videoGameMusic.map((videoGame) => ({
       name: videoGame.title,
       description: videoGame.link,
+      preview: `<img src=${videoGame.thumbnail}>`,
     }))
   );
   open(choice.link);
